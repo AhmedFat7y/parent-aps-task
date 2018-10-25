81f628367779e411f1f9c28cf78d5ffaf27790ff
Nadine Adel <nadine.adel@robustastudio.com>
2018-10-25 17:11:35 +0200

    Import users list component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
